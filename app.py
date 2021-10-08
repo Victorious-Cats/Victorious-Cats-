812d1e64a5091970bc192b57a2c610baea2fa3d8
Duong Quang Minh <s3759757@student.rmit.edu.au>
2021-10-09 01:43:32 +1100

    User emotion input implemented

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session ,url_for,redirect
+import random
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -29,5 +30,48 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("home"))
 
+@app.route("/emote_radio", methods=["POST"])
+def emote_radio():
+    # list of random response, feel free to modify it so it look nicer
+    sad =["Oh No! Lets do something fun today to cheer you up","Don't worry my friend, you alway have me by your side"]
+    happy =["Yay, great to see you feeling good, you make me feel good also","Amazing, it's time for you to spread this positive energy to others"]
+    neutral=["Ok, fair enough","Your day could be even better after you talk with me"]
+    option = int(request.form['emote_radio_output'])
+    
+    message = ""
+    if option < 3:
+        message += random.choice(sad)
+    elif option >3:
+        message += random.choice(happy)
+    else:
+        message += random.choice(neutral)
+    text = f"{message}"
+    return render_template("emote_response.html", text=text)
+
+@app.route("/long_response", methods=["POST"])
+def long_response():
+    #list of positive key words, add more here pls since my vocabulary is limited
+    positive=["good","great","excellent","amazing","happy","perfect","well"]
+    #Same here for negative key words
+    negative=["bad","terrible","ugly","horrible","sad","dissapointed","down"]
+    #Random response for each of them, feel free to add more
+    positive_response=["Oh my god, that's a very cool story from you, I'm so happy to hear that","Yayyy, that's so good, you are such an amazing person, hope you could keep this emotion forerver"]
+    negative_response=["Oops, not a good thing to hear, I suggest you should go watch Squid Game, surely it can make your day better","I'm so sorry to hear that, but I think when you shared it with me, at least you have released your negative energy for the rest of the day"]
+    
+    response = request.form["textbox"]
+    reply=""
+    if any(word in response for word in positive):
+        reply += random.choice(positive_response)
+    elif any(word in response for word in negative):
+        reply += random.choice(positive_response)
+    else:
+        #this is in case no given key words has been found, I dont know what to say so Ima leave it like this, feel free to edit
+        reply += "Interesting!"
+    text = f"{reply}"
+    return render_template("long_response.html", text=text)
+#register user to be complete after I figure out the database
+# def register()
+#also same for comparing user radio input from the past
+# def comparing_input() 
 if __name__=="__main__":
     app.run(debug=True)
